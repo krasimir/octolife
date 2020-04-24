7cd077937f244a81b03a4ecf7d457634fbbd803f
Krasimir Tsonev <krasimir@outset.ws>
2020-04-24 16:19:16 +0300

    Getting all the repos of a user

--- api/data.js
+++ api/data.js
@@ -1,5 +1,6 @@
 const { parse } = require('url');
 const { requestGraphQL } = require('./utils');
+const normalize = require('./normalize');
 
 function JSONResponse(res, data, status = 200) {
   res.setHeader('Content-Type', 'application/json');
@@ -8,7 +9,7 @@ function JSONResponse(res, data, status = 200) {
   res.end(JSON.stringify(data));
 }
 
-const GITHUB_QUERY = `
+const GITHUB_QUERY = (perPage, cursor) => `
 {
   viewer {
     repositories(first: 2) {
@@ -45,6 +46,49 @@ const GITHUB_QUERY = `
 }
 `;
 
+const QUERY_GET_REPOS_OF_ORG = (query, perPage, cursor) => `
+  query {
+    search(query: "${query}", type: REPOSITORY, first: ${perPage}${cursor ? `, after: "${cursor}"` : ''}) {
+      repositoryCount,
+      edges {
+        cursor,
+        node {
+          ... on Repository {
+            name,
+            id,
+            nameWithOwner,
+            isPrivate,
+            owner {
+              login
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+async function getRepos(user) {
+  let perPage = 100;
+  let cursor;
+  let repos = [];
+  const get = async () => {
+    console.log(`Getting repos for ${user} cursor=${cursor}`);
+    const q = QUERY_GET_REPOS_OF_ORG(`user:${user}`, perPage, cursor);
+    const { data } = await requestGraphQL(q);
+
+    repos = repos.concat(data.search.edges);
+
+    if (data.search.repositoryCount > repos.length) {
+      cursor = repos[repos.length - 1].cursor.replace('==', '');
+      return await get();
+    }
+    return repos;
+  };
+
+  return get();
+};
+
 module.exports = async function (req, res) {
   const { query } = parse(req.url, true);
 
@@ -53,12 +97,9 @@ module.exports = async function (req, res) {
   }
 
   try {
-    const data = await requestGraphQL(GITHUB_QUERY);
-    
+    JSONResponse(res, normalize(await getRepos(query.user)));
   } catch(err) {
     console.error(err);
     return JSONResponse(res, { err: err.toString() }, 500)
   }
-  
-  JSONResponse(res, { hello: 'ok' });
 }
