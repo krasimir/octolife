d537f38ed3dfb396dc52222b827292b1098151aa
Krasimir Tsonev <krasimir@outset.ws>
2020-04-25 09:16:45 +0300

    Using a proper timeline graph

--- src/graph2.js
+++ src/graph2.js
@@ -1,59 +1,68 @@
 /* eslint-disable no-shadow, @typescript-eslint/no-use-before-define, no-param-reassign */
+function diffInDays(date1, date2) {
+  const diffTime = Math.abs(date2 - date1);
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+}
 function normalizeDate(str) {
   const d = new Date(str);
   return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
 }
 
+/*
+2008-1-3
+2008-1-4
+2008-1-7
+2008-1-8
+2008-1-10
+*/
+
 export default function graph(user, repos) {
-  repos = repos.map(repo => {
-    const data = [];
-    const commitDates = repo.commits.reduce((res, dateStr) => {
-      const normalizedDate = normalizeDate(dateStr);
-      if (!res) res[normalizedDate] = 0;
-      res[normalizedDate] += 1;
-      return res;
-    }, {});
-    console.log(commitDates);
+  // const normalizedRepos = repos.slice(0, 1).map(repo => {
+  const normalizedRepos = repos.map(repo => {
+    const ranges = [];
+    const commitDates = Object.keys(
+      repo.commits.reduce((r, d) => {
+        const normalizedDate = normalizeDate(d);
+        r[normalizedDate] = true;
+        return r;
+      }, {})
+    )
+      .sort((a, b) => new Date(a).getTime() - new Date(b).getTime())
+      .map(d => new Date(d));
+    let cursor = commitDates.shift();
+    let rangeStart = cursor;
+
+    commitDates.forEach(d => {
+      if (diffInDays(d, cursor) > 1) {
+        ranges.push({
+          range: [normalizeDate(rangeStart), normalizeDate(cursor)],
+          diff: diffInDays(cursor, rangeStart),
+        });
+        rangeStart = d;
+      }
+      cursor = d;
+    });
 
     return {
       label: repo.name,
+      data: ranges.map(r => ({ timeRange: r.range, val: `${r.diff} days` })),
     };
   });
 
-  // console.log(repos);
+  const graphData = [
+    {
+      group: 'Repos',
+      data: normalizedRepos,
+    },
+  ];
+  TimelinesChart()(document.body)
+    .zScaleLabel('My Scale Units')
+    .zQualitative(true)
+    .maxHeight(repos.length * 20)
+    .maxLineHeight(20)
+    .data(graphData);
 
-  // const graphData = [
-  //   {
-  //     group: 'Repos',
-  //     data: [
-  //       {
-  //         label: 'react-in-patterns',
-  //         data: [
-  //           {
-  //             timeRange: [new Date('2008-1-1'), new Date('2008-1-3')],
-  //           },
-  //         ],
-  //       },
-  //       {
-  //         label: 'EventBus',
-  //         data: [
-  //           {
-  //             timeRange: [new Date('2008-1-1'), new Date('2008-1-10')],
-  //           },
-  //           {
-  //             timeRange: [new Date('2008-2-9'), new Date('2008-2-18')],
-  //           },
-  //         ],
-  //       },
-  //     ],
-  //   },
-  // ];
-  // TimelinesChart()(document.body)
-  //   .zScaleLabel('My Scale Units')
-  //   .zQualitative(true)
-  //   .data(graphData);
-
-  // setTimeout(() => {
-  //   document.querySelector('.legend').setAttribute('style', 'display: none');
-  // }, 10);
+  setTimeout(() => {
+    document.querySelector('.legend').setAttribute('style', 'display: none');
+  }, 10);
 }
