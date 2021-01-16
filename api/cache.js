b784059b554bf544599bb51229eee0ee9a134ac9
Krasimir Tsonev <krasimir@outset.ws>
2021-01-16 10:19:02 +0200

    New release

--- api/cache.js
+++ api/cache.js
@@ -1,40 +1,40 @@
 /* eslint-disable camelcase, import/no-dynamic-require  */
-const { parse } = require('url');
-const { json } = require('micro');
-const demo = require('./demo.json');
-const { error, success } = require('./utils');
+const { parse } = require("url");
+const { json } = require("micro");
+const demo = require("./demo.json");
+const { error, success } = require("./utils");
 
 const CACHE_CONTROL = `s-maxage=${60 * 60 * 24 * 90}`;
 
 const cache = {
-  krasimir: demo,
+  // krasimir: demo,
 };
 
 module.exports = async (req, res) => {
   const { query } = parse(req.url, true);
   const { user } = query;
 
-  if (req.method === 'OPTIONS') {
-    return success(res, { hey: 'there' }, 201);
+  if (req.method === "OPTIONS") {
+    return success(res, { hey: "there" }, 201);
   }
 
   if (!user) {
-    return error(res, 'Missing `user` GET param.', 400);
+    return error(res, "Missing `user` GET param.", 400);
   }
 
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const data = await json(req);
     cache[user] = data;
     console.log(`Data for "${user}" cached.`);
-    return success(res, { thanks: 'ok' });
+    return success(res, { thanks: "ok" });
   }
 
   if (cache[user]) {
-    res.setHeader('Cache-Control', CACHE_CONTROL);
+    res.setHeader("Cache-Control", CACHE_CONTROL);
     return success(res, {
       data: cache[user],
       cached: Object.keys(cache),
     });
   }
-  return success(res, { error: 'No data' });
+  return success(res, { error: "No data" });
 };
